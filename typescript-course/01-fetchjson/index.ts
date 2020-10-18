1db4b99d9482373d8380c52e13aefd3d5f463406
Paul Tobin <tobster111@gmail.com>
2020-10-18 17:07:28 +0100

    Adding types to parameters

--- typescript-course/01-fetchjson/index.ts
+++ typescript-course/01-fetchjson/index.ts
@@ -1,8 +1,6 @@
-import axios from 'axios'
+import axios from 'axios';
 
-
-const url = `https://jsonplaceholder.typicode.com/todos/1`
-
+const url = `https://jsonplaceholder.typicode.com/todos/1`;
 interface Todo {
   id: number;
   title: string;
@@ -12,18 +10,17 @@ interface Todo {
 axios.get(url).then((res) => {
   const todo = res.data as Todo;
 
-  const ID = todo.id
-  const title = todo.title
-  const finished = todo.completed
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
 
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
-    The Todo with ID: ${ID}
+    The Todo with ID: ${id}
     Has a title of: ${title}
-    Is it finished? ${finished}
-  `)
-})
-
- 
-
-
-
+    Is it finished? ${completed}
+  `);
+};
