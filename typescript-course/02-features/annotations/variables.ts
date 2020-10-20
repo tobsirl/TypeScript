9232c8019b115ba22e43d31858f7c88257a75b4f
Paul Tobin <tobster111@gmail.com>
2020-10-20 22:07:59 +0100

    Three cases where type inference won't work

--- typescript-course/02-features/annotations/variables.ts
+++ typescript-course/02-features/annotations/variables.ts
@@ -28,3 +28,30 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Functions that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+
+console.log(coordinates);
+
+// 2) When we declare a variable on one line and initalizate it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
