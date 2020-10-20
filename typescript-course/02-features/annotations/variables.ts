6541486cac3dbd29058a3f114867122b0e8f2036
Paul Tobin <tobster111@gmail.com>
2020-10-20 21:46:43 +0100

    Type annotation for an object literals and functions

--- typescript-course/02-features/annotations/variables.ts
+++ typescript-course/02-features/annotations/variables.ts
@@ -17,3 +17,14 @@ let truths: boolean[] = [false, true, false, true];
 class Car {}
 
 let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
