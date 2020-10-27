9add8077d90569d30ad2697b655d9b66d7cae73a
Paul Tobin <tobster111@gmail.com>
2020-10-27 22:40:30 +0000

    Reusable code with interfaces

--- typescript-course/02-features/interfaces.ts
+++ typescript-course/02-features/interfaces.ts
@@ -1,7 +1,25 @@
-interface Vehicle {
-  name: string;
-  year: Date;
-  broken: boolean;
+// interface Vehicle {
+//   name: string;
+//   year: Date;
+//   broken: boolean;
+//   summary(): string;
+// }
+
+// const oldCivic = {
+//   name: 'civic',
+//   year: new Date(),
+//   broken: true,
+//   summary(): string {
+//     return `Name: ${this.name}`;
+//   },
+// };
+
+// const printVehicle = (vehicle: Vehicle): void => {
+//   console.log(vehicle.summary());
+// };
+
+// printVehicle(oldCivic);
+interface Reportable {
   summary(): string;
 }
 
@@ -14,8 +32,18 @@ const oldCivic = {
   },
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(vehicle.summary());
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  },
 };
 
-printVehicle(oldCivic);
+const printSummary = (item: Reportable): string => {
+  return item.summary();
+};
+
+printSummary(oldCivic); //?
+printSummary(drink); //?
