388180eecf3c15fd1275e6464de7b99fe79f42c7
Paul Tobin <tobster111@gmail.com>
2020-10-27 22:15:51 +0000

    Adding a function to the interface

--- typescript-course/02-features/interfaces.ts
+++ typescript-course/02-features/interfaces.ts
@@ -2,18 +2,20 @@ interface Vehicle {
   name: string;
   year: Date;
   broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
   year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken: ${vehicle.broken}`);
+  console.log(vehicle.summary());
 };
 
 printVehicle(oldCivic);
