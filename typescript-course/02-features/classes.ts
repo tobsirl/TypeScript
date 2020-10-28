45b4a7956000faae2a5e7d99a70e271e2b17b277
Paul Tobin <tobster111@gmail.com>
2020-10-28 21:41:04 +0000

    Classes with inheritence, overiding the drive method

--- typescript-course/02-features/classes.ts
+++ typescript-course/02-features/classes.ts
@@ -2,12 +2,18 @@ class Vehicle {
   drive(): void {
     console.log(`chugga chugga`);
   }
-  
+
   honk(): void {
     console.log(`honk`);
   }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+class Car extends Vehicle {
+  drive(): void {
+    console.log(`vroom`)
+  }
+}
+
+const car = new Car();
+car.drive()
+car.honk();
