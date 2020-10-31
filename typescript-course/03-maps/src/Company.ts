04577c43c419140b92cd1915a4cb720676d584cd
Paul Tobin <tobster111@gmail.com>
2020-10-31 18:37:15 +0000

    Adding Company class

--- typescript-course/03-maps/src/Company.ts
+++ typescript-course/03-maps/src/Company.ts
@@ -1,18 +1,19 @@
 import faker from 'faker';
 
-export class User {
+export class Company {
   name: string;
+  catchPhrase: string;
   location: {
     lat: number;
     lng: number;
   };
 
   constructor() {
-    this.name = faker.name.firstName();
+    this.name = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
     };
   }
 }
-
