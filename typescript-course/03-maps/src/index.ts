28f7a8731bc095b9d99f777325e97ee81e7fbe76
Paul Tobin <tobster111@gmail.com>
2020-11-06 11:10:42 +0000

    Creating a custom map to control functionality

--- typescript-course/03-maps/src/index.ts
+++ typescript-course/03-maps/src/index.ts
@@ -1,12 +1,11 @@
-import { User } from './User';
-import { Company } from './Company';
+// import { User } from './User';
+// import { Company } from './Company';
+import 'googlemaps';
 
-const person = new User();
-const company = new Company();
-
-console.log(`Hi There ${person.name}`);
-
-const test = document.querySelector('.test');
-
-test.innerHTML = `<h1>Hi There! ${person.name} lat: ${person.location.lat} long: ${person.location.lng}</h1>
-                <br /><p>${company.name} ${company.catchPhrase} lat: ${company.location.lat} long: ${company.location.lng}</p>`;
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
