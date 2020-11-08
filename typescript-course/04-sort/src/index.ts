183f4fca2909308cb42111c66c897b6e1caff410
Paul Tobin <tobster111@gmail.com>
2020-11-08 12:13:06 +0000

    sort() bubblesort implemented

--- typescript-course/04-sort/src/index.ts
+++ typescript-course/04-sort/src/index.ts
@@ -1,14 +1,26 @@
 class Sorter {
-  constructor(public collection: number[]) {
+  constructor(public collection: number[]) {}
 
-    sort(): void {
+  sort(): void {
+    const { length } = this.collection;
 
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
     }
+  }
 
-    print(): number[] {
-      return this.collection
-    }
+  print(): number[] {
+    return this.collection;
   }
 }
 
-const sorter = new Sorter([10, 3, -5, 0]);
+const sorter = new Sorter([10, 3, -5, 0, 2]);
+sorter.sort();
+
+console.log(sorter.print());
