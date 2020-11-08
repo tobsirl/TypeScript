db2f6707b20a828d1e048a446e016d9c20bef7de
Paul Tobin <tobster111@gmail.com>
2020-11-08 11:44:43 +0000

    Sorter class with method sort()

--- typescript-course/04-sort/build/index.js
+++ typescript-course/04-sort/build/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("Hi there");
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+        sort();
+        void {};
+        print();
+        number[];
+        {
+            return this.collection;
+        }
+    }
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
--- typescript-course/04-sort/src/index.ts
+++ typescript-course/04-sort/src/index.ts
@@ -1,1 +1,14 @@
-console.log(`Hi there`);
+class Sorter {
+  constructor(public collection: number[]) {
+
+    sort(): void {
+
+    }
+
+    print(): number[] {
+      return this.collection
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
