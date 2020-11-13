a9d572e1b0bad558562b588f6e9f619e9f210536
Paul Tobin <tobster111@gmail.com>
2020-11-13 20:40:58 +0000

    Sorting strings

--- typescript-course/04-sort/build/index.js
+++ typescript-course/04-sort/build/index.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
-var NumbersCollection_1 = require("./NumbersCollection");
-var numbers = new NumbersCollection_1.NumbersCollection([12345, 10, 3, -5, 0, 2]);
-var sorter = new Sorter_1.Sorter(numbers);
+var CharactersCollection_1 = require("./CharactersCollection");
+// const numbers = new NumbersCollection([12345, 10, 3, -5, 0, 2]);
+// const sorter = new Sorter(numbers);
+var characters = new CharactersCollection_1.CharactersCollection('aFeerGsergehtr');
+var sorter = new Sorter_1.Sorter(characters);
 sorter.sort();
-console.log(numbers.data);
+console.log(characters.data);
--- typescript-course/04-sort/src/index.ts
+++ typescript-course/04-sort/src/index.ts
@@ -1,9 +1,14 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbers = new NumbersCollection([12345, 10, 3, -5, 0, 2]);
-const sorter = new Sorter(numbers);
+// const numbers = new NumbersCollection([12345, 10, 3, -5, 0, 2]);
+// const sorter = new Sorter(numbers);
+
+const characters = new CharactersCollection('aFeerGsergehtr');
+
+const sorter = new Sorter(characters);
 
 sorter.sort();
 
-console.log(numbers.data);
+console.log(characters.data);
