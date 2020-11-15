32149ec4eed5281ce136a80de0496b499dc2b4f6
Paul Tobin <tobster111@gmail.com>
2020-11-15 15:31:45 +0000

    Using Sorter to sort the linkedlist

--- typescript-course/04-sort/src/index.ts
+++ typescript-course/04-sort/src/index.ts
@@ -1,14 +1,26 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const numbers = new NumbersCollection([12345, 10, 3, -5, 0, 2]);
-const sorterNumbers = new Sorter(numbers);
+// const numbers = new NumbersCollection([12345, 10, 3, -5, 0, 2]);
+// const sorterNumbers = new Sorter(numbers);
 
-const characters = new CharactersCollection('aFeerGsergehtr');
+// const characters = new CharactersCollection('aFeerGsergehtr');
 
-const sorter = new Sorter(characters);
+// const sorter = new Sorter(characters);
 
+// sorter.sort();
+
+// console.log(characters.data);
+
+const linkedList = new LinkedList();
+
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-
-console.log(characters.data);
+linkedList.print();
