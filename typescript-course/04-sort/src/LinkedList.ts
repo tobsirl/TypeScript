e25137d943354002b875ac1bbf49fbfdd834946c
Paul Tobin <tobster111@gmail.com>
2020-11-15 15:02:52 +0000

    Implementation of add and lenght

--- typescript-course/04-sort/src/LinkedList.ts
+++ typescript-course/04-sort/src/LinkedList.ts
@@ -6,7 +6,37 @@ class Node {
 export class LinkedList {
   head: Node | null = null;
 
-  add() {
+  add(data: number): void {
+    const node = new Node(data);
 
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let tail = this.head;
+    // cycle through the list until it hits null - next = undefined
+    while (tail.next) {
+      tail = tail.next;
+    }
+
+    // point to the new node
+    tail.next = node;
+  }
+
+  get length(): number {
+    if (!this.head) {
+      return 0;
+    }
+
+    let length = 1;
+    let node = this.head;
+
+    while (node.next) {
+      length++;
+      node = node.next;
+    }
+    
+    return length;
   }
 }
