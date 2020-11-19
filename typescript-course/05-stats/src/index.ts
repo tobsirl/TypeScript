c498b3f2606fc080409ae0d57415e57a439a93de
Paul Tobin <tobster111@gmail.com>
2020-11-19 22:24:01 +0000

    Creating a class for CsvFileReader

--- typescript-course/05-stats/src/index.ts
+++ typescript-course/05-stats/src/index.ts
@@ -1,14 +1,10 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('./football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+// const matches =
 
+const csvFileReader = new CsvFileReader('football.csv');
+
+csvFileReader.read();
 // enum - enumeration
 enum MatchResult {
   HomeWin = 'H',
@@ -18,7 +14,7 @@ enum MatchResult {
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of csvFileReader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
