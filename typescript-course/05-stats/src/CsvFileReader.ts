c1314670237f4a448715ed282cd6e392ea7e58be
Paul Tobin <tobster111@gmail.com>
2020-11-20 17:04:53 +0000

    Using a tuple to store the data

--- typescript-course/05-stats/src/CsvFileReader.ts
+++ typescript-course/05-stats/src/CsvFileReader.ts
@@ -3,8 +3,10 @@ import fs from 'fs';
 import { dateStringToDate } from './utils';
 import { MatchResult } from './matchResult';
 
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 export class CsvFileReader {
-  data: string[][] = [];
+  data: MatchData[] = [];
 
   constructor(public filename: string) {}
 
@@ -17,16 +19,18 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map((row: string[]): any => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult,
-          row[6],
-        ];
-      });
+      .map(
+        (row: string[]): MatchData => {
+          return [
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult,
+            row[6],
+          ];
+        }
+      );
   }
 }
