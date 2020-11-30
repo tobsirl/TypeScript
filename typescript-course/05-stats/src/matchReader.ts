c0b65107034b3dd0e65f7fc91311fa2417d12452
Paul Tobin <tobster111@gmail.com>
2020-11-30 16:04:53 +0000

    Removing references to MatchData in CSVFileReader

--- typescript-course/05-stats/src/matchReader.ts
+++ typescript-course/05-stats/src/matchReader.ts
@@ -1,10 +1,27 @@
+import { dateStringToDate } from './utils';
+import { MatchResult } from './matchResult';
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 interface DataReader {
   read(): void;
   data: string[][];
 }
 
 export class MatchReader {
-  constructor(public reader: DataReader) {
+  constructor(public reader: DataReader) {}
 
-  }
+  // .map(
+  //   (row: string[]): MatchData => {
+  //     return [
+  //       dateStringToDate(row[0]),
+  //       row[1],
+  //       row[2],
+  //       parseInt(row[3]),
+  //       parseInt(row[4]),
+  //       row[5] as MatchResult,
+  //       row[6],
+  //     ];
+  //   }
+  // );
 }
