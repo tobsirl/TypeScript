341d6e814790ba73b091282fd19b57de0ce90417
Paul Tobin <tobster111@gmail.com>
2020-12-05 17:18:58 +0000

    Using static methods to clean up the code in index.ts

--- typescript-course/05-stats/src/index.ts
+++ typescript-course/05-stats/src/index.ts
@@ -1,16 +1,9 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
 import { Summary } from './Summary';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { HtmlReport } from './reportTargets/HtmlReport';
 
-// Create an object that satisfies the "DataReader" interface
-const csvFileReader = new CsvFileReader('football.csv');
-// Create an instance of MatchReader and pass in something satisfying the "DataReader" interface
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Chelsea'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
 summary.buildAndPrintReport(matchReader.matches);
