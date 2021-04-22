46084dec38d0d256bfeb75c44de5d2f74affc029
Paul Tobin <tobster111@gmail.com>
2021-04-22 21:47:28 +0100

    Working with arrays

--- NOBSTS/ts-basics/basics.ts
+++ NOBSTS/ts-basics/basics.ts
@@ -8,3 +8,8 @@ console.log(userName);
 let myNumber: number = 10;
 
 let myRegex: RegExp = /foo/;
+
+const names: string[] = userName.split(' ');
+
+const [first, last] = names;
+const myValues: Array<number> = [1, 2, 3];
