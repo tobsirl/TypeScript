90e1fe4a8a6978a4a6e5ee7e824317578a7c4fb3
Paul Tobin <tobster111@gmail.com>
2021-04-24 10:01:32 +0100

    Example of maps and loops

--- NOBSTS/ts-basics/basics.ts
+++ NOBSTS/ts-basics/basics.ts
@@ -28,4 +28,18 @@ const myPerson: Person = {
   middleName: 'John',
 };
 
-console.log(myPerson['lastName'])
+// Maps
+const ids: Record<number, string> = {
+  10: 'a',
+  20: 'b',
+};
+
+ids[30] = 'c';
+
+// Loops
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+[1, 2, 3].forEach((v) => console.log(v));
+const result: number[] = [4, 5, 6].map((v) => v * 10);
