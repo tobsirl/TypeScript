528e0cfa68952f2109890cdcd2f4ff89166e01f5
Paul Tobin <tobster111@gmail.com>
2021-05-22 16:54:57 +0100

    functions with params

--- NOBSTS/ts-basics/funcs-and-funcs.ts
+++ NOBSTS/ts-basics/funcs-and-funcs.ts
@@ -2,3 +2,12 @@ export function printToFile(text: string, callback: () => void): void {
   console.log(text);
   callback();
 }
+
+export function arrayMutate(
+  numbers: number[],
+  mutate: (v: number) => number
+): number[] {
+  return numbers.map(mutate);
+}
+
+arrayMutate([1, 2, 3], (v) => v * 2); //?
