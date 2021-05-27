7457845722f5cfe9f141e7a13bb141953f3f5567
Paul Tobin <tobster111@gmail.com>
2021-05-27 19:11:01 +0100

    Overloading for a string

--- NOBSTS/ts-basics/parseCoordinate.ts
+++ NOBSTS/ts-basics/parseCoordinate.ts
@@ -16,13 +16,19 @@ function parseCoordinateFromNumbers(x: number, y: number): Coordinate {
 
 function parseCoordinate(obj: Coordinate): Coordinate;
 function parseCoordinate(x: number, y: number): Coordinate;
+function parseCoordinate(str: string): Coordinate;
 function parseCoordinate(arg1: unknown, arg2?: unknown): Coordinate {
   let coord: Coordinate = {
     x: 0,
     y: 0,
   };
 
-  if (typeof arg1 === 'object') {
+  if (typeof arg1 === 'string') {
+    (arg1 as string).split(',').forEach((str) => {
+      const [key, value] = str.split(':');
+      coord[key as 'x' | 'y'] = parseInt(value, 10);
+    });
+  } else if (typeof arg1 === 'object') {
     coord = {
       ...(arg1 as Coordinate),
     };
@@ -38,3 +44,4 @@ function parseCoordinate(arg1: unknown, arg2?: unknown): Coordinate {
 
 parseCoordinate(10, 20); //?
 parseCoordinate({ x: 10, y: 34 }); //?
+parseCoordinate('x: 12, y: 23'); //?
