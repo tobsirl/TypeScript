075bdc0405e43ad9a43affb1d819723f11783acd
Paul Tobin <tobster111@gmail.com>
2021-05-29 16:00:13 +0100

    Optional fields

--- NOBSTS/ts-basics/optionals.ts
+++ NOBSTS/ts-basics/optionals.ts
@@ -1,6 +1,32 @@
 // optional parameters
-function printIngredient(quantity: string, ingredient: string) {
-  console.log(`${quantity} ${ingredient}`);
+function printIngredient(
+  quantity: string,
+  ingredient: string,
+  extra?: string,
+  required?: string
+) {
+  console.log(
+    `${quantity} ${ingredient} ${extra ? ` ${extra}` : ''} ${
+      required ? ` ${required}` : ''
+    }`
+  );
 }
 
 printIngredient('1C', 'Floor');
+printIngredient('1C', 'Floor', 'something more');
+printIngredient('1C', 'Floor', 'something more', 'another something');
+
+// optional fields
+interface User {
+  id: string;
+  info?: {
+    email?: string;
+  };
+}
+
+function getEmail(user: User): string {
+  if (user.info) {
+    return user.info.email;
+  }
+  return '';
+}
