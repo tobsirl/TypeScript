ae8a8370cc6620797d90ac252f92c419f5ad245d
Paul Tobin <tobster111@gmail.com>
2021-06-02 18:20:15 +0100

    Using generic types

--- NOBSTS/ts-basics/generics.ts
+++ NOBSTS/ts-basics/generics.ts
@@ -1,11 +1,15 @@
-function simpleStringState(
-  initial: string
-): [() => string, (v: string) => void] {
-  let str: string = initial;
+// Making a generic function
+function simpleState<T>(initial: T): [() => T, (v: T) => void] {
+  let val: T = initial;
   return [
-    () => str,
-    (v: string) => {
-      str = v;
+    () => val,
+    (v: T) => {
+      val = v;
     },
   ];
 }
+
+const [st1getter, st1setter] = simpleState(10);
+console.log(st1getter());
+st1setter(62);
+console.log(st1getter());
