7c4d20096ea72bb814576c2cdef60920eaf7fb94
Paul Tobin <tobster111@gmail.com>
2021-06-02 18:23:35 +0100

    Overring inferred generic types

--- NOBSTS/ts-basics/generics.ts
+++ NOBSTS/ts-basics/generics.ts
@@ -13,3 +13,9 @@ const [st1getter, st1setter] = simpleState(10);
 console.log(st1getter());
 st1setter(62);
 console.log(st1getter());
+
+// Overring inferred generic types
+const [st2getter, st2setter] = simpleState<string | null>(null);
+console.log(st2getter());
+st2setter('string');
+console.log(st2getter());
