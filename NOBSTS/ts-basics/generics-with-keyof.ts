a0663577eb4430730335a89f9113410f5a57a34c
Paul Tobin <tobster111@gmail.com>
2021-06-05 16:57:48 +0100

    Event map

--- NOBSTS/ts-basics/generics-with-keyof.ts
+++ NOBSTS/ts-basics/generics-with-keyof.ts
@@ -13,3 +13,30 @@ const dogs = [
 
 pluck(dogs, 'name');
 pluck(dogs, 'age');
+
+// event map
+interface BaseEvent {
+  time: number;
+  user: string;
+}
+
+interface EventMap {
+  addToCart: BaseEvent & { quantity: number; productID: string };
+  checkout: BaseEvent;
+}
+
+function sendEvent<Name extends keyof EventMap>(
+  name: Name,
+  data: EventMap[Name]
+): void {
+  console.log([name, data]);
+}
+
+sendEvent('addToCart', {
+  productID: '123',
+  user: 'baz',
+  quantity: 1,
+  time: 10,
+});
+
+sendEvent('checkout', { user: 'Philip', time: 14 });
