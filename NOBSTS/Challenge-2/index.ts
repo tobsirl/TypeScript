b706f0685d9986337592405f4184a863212536f6
Paul Tobin <tobster111@gmail.com>
2021-06-09 14:47:10 +0100

    map using reduce in typescript using generics

--- NOBSTS/Challenge-2/index.ts
+++ NOBSTS/Challenge-2/index.ts
@@ -14,3 +14,10 @@ function myFilter<T>(items: T[], filterFunc: (v: T) => boolean): T[] {
 }
 
 myFilter([1, 2, 3, 4, 5, 6, 7, 8], (v) => v % 2 === 0);
+
+// map
+function myMap<T, K>(items: T[], mapFunc: (v: T) => K): K[] {
+  return items.reduce((a, v) => [...a, mapFunc(v)], [] as K[]);
+}
+
+myMap([1, 2, 3, 4, 5, 6, 7, 8], (v) => (v * 10).toString());
