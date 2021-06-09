ddfd85d6d253724ec0657f4560cd542466bd3d83
Paul Tobin <tobster111@gmail.com>
2021-06-09 14:36:57 +0100

    filter using reduce in typescript using generics

--- NOBSTS/Challenge-2/index.ts
+++ NOBSTS/Challenge-2/index.ts
@@ -1,3 +1,4 @@
+// forEach
 function myForEach<T>(items: T[], forEachFunc: (v: T) => void): void {
   items.reduce((a, v) => {
     forEachFunc(v);
@@ -6,3 +7,10 @@ function myForEach<T>(items: T[], forEachFunc: (v: T) => void): void {
 }
 
 myForEach(['a', 'b', 'c'], (v) => console.log(`forEach ${v}`));
+
+// filter
+function myFilter<T>(items: T[], filterFunc: (v: T) => boolean): T[] {
+  return items.reduce((a, v) => (filterFunc(v) ? [...a, v] : a), []);
+}
+
+myFilter([1, 2, 3, 4, 5, 6, 7, 8], (v) => v % 2 === 0);
