03927c545439ff67ed6a15181c5145b235a2ff9c
Paul Tobin <tobster111@gmail.com>
2021-06-10 10:44:42 +0100

    Utility types - Partials - all types set ot optional

--- NOBSTS/ts-basics/utility-types-1.ts
+++ NOBSTS/ts-basics/utility-types-1.ts
@@ -1,14 +1,12 @@
+// difficult to add new fields
 interface MyUser {
   name: string;
   id: string;
   email?: string;
+  phone?: string;
 }
 
-interface MyUserOptionals {
-  name?: string;
-  id?: string;
-  email?: string;
-}
+type MyUserOptionals = Partial<MyUser>;
 
 const merge = (user: MyUser, overrides: MyUserOptionals): MyUser => {
   return {
@@ -21,3 +19,6 @@ merge(
   { name: 'Jack', id: 'foo', email: 'jack@gmail.com' },
   { name: 'notJack', id: 'notFoo', email: 'notjack@gmail.com' }
 );
+
+// partial
+// Constructs a type with all properties of Type set to optional. This utility will return a type that represents all subsets of a given type.
