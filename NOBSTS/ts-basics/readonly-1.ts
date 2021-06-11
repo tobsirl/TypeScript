629974a08d385a0a7328a09fdaeefb0b2c9ce43b
Paul Tobin <tobster111@gmail.com>
2021-06-11 10:29:26 +0100

    readonly tuples

--- NOBSTS/ts-basics/readonly-1.ts
+++ NOBSTS/ts-basics/readonly-1.ts
@@ -3,8 +3,6 @@ interface Cat {
   breed: string;
 }
 
-type ReadonlyCat = Readonly<Cat>;
-
 function makeCat(name: string, breed: string): Readonly<Cat> {
   return {
     name,
@@ -13,4 +11,15 @@ function makeCat(name: string, breed: string): Readonly<Cat> {
 }
 
 const usul = makeCat('Usul', 'Tabby');
-usul.name = 'Piter';
+// usul.name = 'Piter';
+
+function makeCoordinate(
+  x: number,
+  y: number,
+  z: number
+): readonly [number, number, number] {
+  return [x, y, z];
+}
+
+const c1 = makeCoordinate(10, 20, 30);
+c1[0] = 15
