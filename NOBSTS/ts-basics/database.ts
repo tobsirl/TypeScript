cb6fd0f8352ab546fb1e1dacbbadf1185b8ef08c
Paul Tobin <tobster111@gmail.com>
2021-06-13 11:18:56 +0100

    Implement PersistentMemoryDB

--- NOBSTS/ts-basics/database.ts
+++ NOBSTS/ts-basics/database.ts
@@ -3,17 +3,34 @@ interface Database {
   set(id: string, value: string): void;
 }
 
+interface Persistable {
+  saveToString(): string;
+  restoreFromString(storedState: string): void;
+}
+
 class InMemoryDatabase implements Database {
-  private db: Record<string, string> = {};
+  protected db: Record<string, string> = {};
   get(id: string): string {
     return this.db[id];
   }
+
   set(id: string, value: string): void {
     this.db[id] = value;
   }
 }
 
-const myDB = new InMemoryDatabase();
+class PersistentMemoryDB extends InMemoryDatabase implements Persistable {
+  saveToString(): string {
+    return JSON.stringify(this.db);
+  }
+
+  restoreFromString(storedState: string): void {
+    this.db = JSON.parse(storedState);
+  }
+}
+
+const myDB = new PersistentMemoryDB();
 myDB.set('foo', 'bar');
 // myDB.db; //?
 myDB.get('foo'); //?
+myDB.saveToString(); //?
