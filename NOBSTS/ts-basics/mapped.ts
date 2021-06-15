b8d314fb1791760afcedb4e2332e09b4920ebc57
Paul Tobin <tobster111@gmail.com>
2021-06-15 20:15:16 +0100

    Mapping types

--- NOBSTS/ts-basics/mapped.ts
+++ NOBSTS/ts-basics/mapped.ts
@@ -18,3 +18,23 @@ type OptionsFlags<Type> = {
 };
 
 type DogInfoOptions = OptionsFlags<DogInfo>;
+
+type Listeners<Type> = {
+  [Property in keyof Type]: (newValue: Type[Property]) => void;
+};
+
+function listenToObject<T>(obj: T, listeners: Listeners<T>): void {
+  throw 'Needs to be implemented';
+}
+
+const lg: DogInfo = {
+  name: 'LG',
+  age: 13,
+};
+
+type DogInfoListeners = Listeners<DogInfo>;
+
+listenToObject(lg, {
+  onNameChange: (v: string) => {},
+  onAgeChange: (v: number) => {},
+});
