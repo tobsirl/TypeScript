0aee2a039d7755a19851a306ead22c0a426543a6
Paul Tobin <tobster111@gmail.com>
2021-06-15 19:58:30 +0100

    Added DogInfo

--- NOBSTS/ts-basics/mapped.ts
+++ NOBSTS/ts-basics/mapped.ts
@@ -1,8 +1,19 @@
 type MyFlexibleDogInfo = {
   name: string;
-} & Record<string, string>;
+  [key: string]: string | number;
+};
 
 const dog: MyFlexibleDogInfo = {
   name: 'LG',
   breed: 'Mutt',
 };
+
+interface DogInfo {
+  name: string;
+  age: number;
+}
+
+type OptionsFlags<Type> = {
+  [Property in keyof Type]: boolean;
+};
+
