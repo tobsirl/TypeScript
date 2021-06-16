0784f0fecb2ca62ef2676981b714f77898729553
Paul Tobin <tobster111@gmail.com>
2021-06-16 18:40:42 +0100

    Static Member fields

--- NOBSTS/ts-basics/readonly-2.ts
+++ NOBSTS/ts-basics/readonly-2.ts
@@ -1,7 +1,15 @@
 class Doggy {
-  constructor(public readonly name: string, public age: number) {}
+  constructor(public readonly name: string, public readonly age: number) {}
 }
 
 const rex = new Doggy('Rex', 13);
-rex.name = 'foo';
+// rex.name = 'foo';
 rex;
+
+class DogList {
+  private doggies: Doggy[] = [];
+
+  public instance: DogList = new DogList();
+}
+
+DogList.instance;
