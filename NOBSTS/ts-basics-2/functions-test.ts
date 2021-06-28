1a3b5daa3ea6d8aff55a73c09cc040003cb86e4d
Paul Tobin <tobster111@gmail.com>
2021-06-28 20:21:43 +0100

    Union type

--- NOBSTS/ts-basics-2/functions-test.ts
+++ NOBSTS/ts-basics-2/functions-test.ts
@@ -1,6 +1,7 @@
-import addNumbers, { addStrings } from './functions';
+import addNumbers, { addStrings, format } from './functions';
 
 addNumbers(1, 2);
 addNumbers(1, 3);
 
 addStrings('Jack', 'Gill');
+addStrings('Jack');
--- NOBSTS/ts-basics-2/functions.ts
+++ NOBSTS/ts-basics-2/functions.ts
@@ -4,5 +4,8 @@ function addNumbers(a: number, b: number): number {
 
 export default addNumbers;
 
-export const addStrings = (str1: string, str2: string): string =>
+export const addStrings = (str1: string, str2: string = ''): string =>
   `${str1} ${str2}`;
+
+export const format = (title: string, param: string | number): string =>
+  `${title} ${param}`;
