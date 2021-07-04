80ac487450c9cc0dd8cfd9559f05aa2d7d7b0790
Paul Tobin <tobster111@gmail.com>
2021-07-04 15:22:52 +0100

    Using unknown

--- NOBSTS/ts-basics-2/parseCoordinate.ts
+++ NOBSTS/ts-basics-2/parseCoordinate.ts
@@ -18,6 +18,25 @@ function parseCoordinateFromNumbers(x: number, y: number): Coordinate {
 
 function parseCoordinate(obj: Coordinate): Coordinate;
 function parseCoordinate(x: number, y: number): Coordinate;
-function parseCoordinate(arg1: unknown, arg2: unknown): Coordinate {
-  
+function parseCoordinate(arg1: unknown, arg2?: unknown): Coordinate {
+  let coord: Coordinate = {
+    x: 0,
+    y: 0,
+  };
+
+  if (typeof arg1 === 'object') {
+    coord = {
+      ...(arg1 as Coordinate),
+    };
+  } else {
+    coord = {
+      x: arg1 as number,
+      y: arg2 as number,
+    };
+  }
+
+  return coord;
 }
+
+parseCoordinate(10, 20);
+parseCoordinate({ x: 52, y: 35 });
