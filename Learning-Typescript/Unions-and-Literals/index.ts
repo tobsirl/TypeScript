fa34ee6b79cde7ccf0b7cfd842cce922aa4d07cc
Paul Tobin <tobster111@gmail.com>
2022-07-20 21:14:35 +0100

    typeof checks

--- Learning-Typescript/Unions-and-Literals/index.ts
+++ Learning-Typescript/Unions-and-Literals/index.ts
@@ -55,3 +55,10 @@ if (scientist === 'Rosalind Franklin') {
 scientist.toUpperCase();
 // Error: Property 'toUpperCase' does not exist on type 'string | number'.
 // Property 'toUpperCase' does not exist on type 'number'.
+
+// Typeof Checks
+let researcher = Math.random() > 0.5 ? 'Rosalind Franklin' : 51;
+
+if (typeof researcher === 'string') {
+  researcher.toUpperCase(); // Ok: string
+}
