82df06a872bf92f564e73c83927091104f7bc793
Paul Tobin <tobster111@gmail.com>
2022-07-20 18:27:18 +0100

    physicist

--- Learning-Typescript/Unions-and-Literals/index.ts
+++ Learning-Typescript/Unions-and-Literals/index.ts
@@ -12,3 +12,13 @@ if (Math.random() > 0.5) {
  * The order of a union type does not matter. You can write boolean | number or number |
  * boolean and TypeScript will treat both the exact same.
  */
+
+// Union Properties
+let physicist = Math.random() > 0.5 ? 'Marie Curie' : 84;
+
+physicist.toString();
+physicist.toUpperCase();
+/**
+ * Error: Property 'toUpperCase' does not exist on type 'string | number'.
+ * Property 'toUpperCase does not exist on type 'number'.
+ */
