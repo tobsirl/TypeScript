3bcba8e04376ca0114ef75c239a6ce62ababfb8c
Paul Tobin <tobster111@gmail.com>
2022-07-20 18:34:49 +0100

    admiral.toFixed()

--- Learning-Typescript/Unions-and-Literals/index.ts
+++ Learning-Typescript/Unions-and-Literals/index.ts
@@ -28,3 +28,11 @@ physicist.toFixed();
  * Error: Property 'toFixed' does not exist on type 'string | number'.
  * Property 'toFixed' does not exist on type 'string.
  */
+
+// Narrowing
+let admiral: number | string;
+admiral = 'Grace Hopper';
+
+admiral.toUpperCase();
+admiral.toFixed();
+// Error: Property 'toFixed' does not exist on type 'string'.
