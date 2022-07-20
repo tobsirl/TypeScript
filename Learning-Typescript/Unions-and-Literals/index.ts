aa2cca888b5d426d267ffbc31e7c33fa3407b83e
Paul Tobin <tobster111@gmail.com>
2022-07-20 18:22:52 +0100

    thinker

--- Learning-Typescript/Unions-and-Literals/index.ts
+++ Learning-Typescript/Unions-and-Literals/index.ts
@@ -1,2 +1,14 @@
 // Union Types
 let mathematician = Math.random() > 0.5 ? undefined : 'Mark Goldberg';
+
+let thinker: string | null = null;
+
+if (Math.random() > 0.5) {
+  thinker = 'Susanne Langer'; // Ok
+}
+
+/**
+ * Note
+ * The order of a union type does not matter. You can write boolean | number or number |
+ * boolean and TypeScript will treat both the exact same.
+ */
