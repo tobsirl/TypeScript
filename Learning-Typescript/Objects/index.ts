64d2cac0e80445db10a753424144e1f68868739a
Paul Tobin <tobster111@gmail.com>
2022-07-24 10:42:21 +0100

    Optional Properties

--- Learning-Typescript/Objects/index.ts
+++ Learning-Typescript/Objects/index.ts
@@ -167,3 +167,34 @@ const poemMismatch1: Poem1 = {
   name: 'Tulips',
 }
 
+// Optional Properties
+type Book = {
+  author?: string;
+  pages: number;
+}
+
+// Ok
+const ok: Book = {
+  author: 'J.R.R. Tolkien',
+  pages: 1254,  
+}
+
+const missing: Book = {
+  author: 'J.R.R. Tolkien',
+}
+
+// Error: Property 'pages' is missing in type '{ author: string; }' 
+// but required in type 'Book'
+
+type Writers = {
+  author: string | undefined;
+  editor?: string;
+}
+
+// Ok: author is provided as undefined
+const hasRequired: Writers = {
+  author: undefined
+}
+
+const missingRequired: Writers = {}
+// Error: Property 'author' is missing in type '{}' but required in type 'Writers'
