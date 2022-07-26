695e0625272783912feb861b53a31248a094c34c
Paul Tobin <tobster111@gmail.com>
2022-07-26 19:36:00 +0100

    oneArt

--- Learning-Typescript/Objects/index.ts
+++ Learning-Typescript/Objects/index.ts
@@ -312,3 +312,16 @@ const morningGlory: ShortPoem = {
   kigo: 'Morning Glory',
   type: 'haiku',
 }
+
+const oneArt: ShortPoem = {
+  author: 'Elizabeth Bishop',
+  type: 'villanelle',
+}
+/**
+ * Error: Type '{ author: string; type: 'villanelle'; }' 
+ * is not assignable to type 'ShortPoem'.
+ * Type '{ author: string; type: 'villanelle'; }' is not assignable to
+ * type '{ author: string; } & { meter: number; type: 'villanelle'; }',
+ * Property 'meter' is missing in type '{ author: string; type: 'villanelle'; }'
+ * but required in type '{ meter: number; type: 'villanelle'; }'
+ */
