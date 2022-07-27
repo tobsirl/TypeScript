d48dce8a89f868464b12e2bd92f2657a8ac772ac
Paul Tobin <tobster111@gmail.com>
2022-07-27 18:20:34 +0100

    function singTwo

--- Learning-Typescript/Functions/index.ts
+++ Learning-Typescript/Functions/index.ts
@@ -1,4 +1,9 @@
-// Function Parameters
+// * Function Parameters
 function sing(song: string) {
   console.log(`Singing ${song}`);
+}
+
+// * Required Parameters
+function singTwo(first: string, second: string) {
+  console.log(`Singing ${first} and ${second}`);
 }
