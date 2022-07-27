987872f5e15544ea4d8f87479b1ce3e91af9591f
Paul Tobin <tobster111@gmail.com>
2022-07-27 17:59:42 +0100

    oneArt

--- Learning-Typescript/Objects/index.ts
+++ Learning-Typescript/Objects/index.ts
@@ -331,3 +331,8 @@ type ShortPoemBase = { author: string };
 type Haiku = ShortPoemBase & { kigo: string; type: 'haiku' };
 type Villanelle = ShortPoemBase & { meter: number; type: 'villanelle' };
 type ShortPoem = Haiku | Villanelle;
+
+const oneArt: ShortPoem = {
+  author: "Elizabeth Bishop",
+  type: "villanelle",
+}
