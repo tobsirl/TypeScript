0c3f7a1a2bb2825de20346fc0341d38cd9bc5087
Paul Tobin <tobster111@gmail.com>
2022-07-28 18:55:31 +0100

    Using rateSong

--- Learning-Typescript/Functions/index.ts
+++ Learning-Typescript/Functions/index.ts
@@ -53,3 +53,7 @@ function announceSinger(singer?: string, song: string) {}
 function rateSong(song: string, rating = 0) {
   console.log(`${song} gets ${rating}/5 stars`);
 }
+
+rateSong('Photograph'); // Ok
+rateSong('Set Fire to the Rain', 5); // Ok
+rateSong('Set Fire to the Rain', undefined); // Ok
