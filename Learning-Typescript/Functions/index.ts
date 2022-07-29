3a594811ec04342bb366d611922360215a10a9a6
Paul Tobin <tobster111@gmail.com>
2022-07-29 18:54:24 +0100

    getSongRecordingDate

--- Learning-Typescript/Functions/index.ts
+++ Learning-Typescript/Functions/index.ts
@@ -97,3 +97,17 @@ function singSongsRecursive(songs: string[], count = 0): number {
 
 const singSongsRecursiveArrow = (songs: string[], count = 0): number =>
   songs.length ? singSongsRecursiveArrow(songs.slice(1), count + 1) : count;
+
+function getSongRecordingDate(song: string): Date | undefined {
+  switch (song) {
+    case 'Strange Fruit':
+      return new Date('April 20, 1939'); // Ok
+
+    case 'Greensleeves':
+      return 'unknown';
+    // Error: Type 'string' is not assignable to type 'Date'.
+    
+    default:
+      return undefined; // Ok
+  }
+}
