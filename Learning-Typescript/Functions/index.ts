a46f7cc48588eb8bb5b9d06a3a1734becd732900
Paul Tobin <tobster111@gmail.com>
2022-07-30 11:28:35 +0100

    logSong

--- Learning-Typescript/Functions/index.ts
+++ Learning-Typescript/Functions/index.ts
@@ -123,3 +123,19 @@ function runOnSongs(getSongAt: (index: number) => string) {
     console.log(getSongAt(i));
   }
 }
+
+function getSongAt(index: number) {
+  return `${songs[index]}`;
+}
+
+runOnSongs(getSongAt);
+
+function logSong(song: string) {
+  return `${song}`;
+}
+
+runOnSongs(logSong);
+// Error: Argument of type '(song: string) => string' is not 
+// assignable to parameter of type '(index: number) => string'.
+// Types of parameters 'song' and 'index' are incompatible.
+// Type 'number' is not assignable to type 'string'.
