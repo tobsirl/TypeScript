9eac755a888f112d3105ab2f66f11fc4bb718714
Paul Tobin <tobster111@gmail.com>
2022-08-01 17:03:33 +0100

    Call signature compatibility

--- Learning-Typescript/Functions/index.ts
+++ Learning-Typescript/Functions/index.ts
@@ -241,3 +241,14 @@ createDate(7, 27, 1987); // Ok
 createDate(4, 1);
 // Error: No overload expects 2 arguments, but over loads
 // do exist that expect either 1 or 3 arguments.
+
+// * Call-Signature Compatibility
+function format(data: string): string; // Ok
+function format(data: string, needle: string, haystack: string): string; // Ok
+
+function format(getData: () => string): string;
+// This overload signature is not compatible with its implementation signature.
+
+function format(data: string, needle?: string, haystack?: string): string {
+  return needle && haystack ? data.replace(needle, haystack) : data;
+}
