71c13d5ecde09354704ab8364a23bbdfd00f4c04
Paul Tobin <tobster111@gmail.com>
2022-08-02 18:42:08 +0100

    Array with different types

--- Learning-Typescript/Arrays/index.ts
+++ Learning-Typescript/Arrays/index.ts
@@ -15,3 +15,13 @@ let createString: () => string[];
 
 // Type is an array of functions that each return a string
 let stringCreators: (() => string)[];
+
+// * Union-Types Arrays
+// Types is either a number or an array of strings
+let stringOrArrayOfNumber: string | number[];
+
+// Type is an array of elements that are either number or a string
+let arrayOfStringOrNumber: (string | number)[];
+
+// Type is (string | undefined)[]
+const namesMaybe = ['John', 'Jane', undefined];
