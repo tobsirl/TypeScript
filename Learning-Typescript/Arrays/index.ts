2c504614d0d1f7c4b03bfd75cd5aea001a5c1119
Paul Tobin <tobster111@gmail.com>
2022-08-02 18:06:08 +0100

    Breaking array types

---
+++ Learning-Typescript/Arrays/index.ts
@@ -0,0 +1,6 @@
+// * ARRAYS
+const warriors = ['Superman', 'Batman', 'Flash'];
+warriors.push('Antman'); // Ok
+
+warriors.push(true);
+// Error: Argument of type 'true' is not assignable to parameter of type 'string'.
