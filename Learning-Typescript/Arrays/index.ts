0cdf8c569c1f01cfd2fa65fb92bdee62b3151746
Paul Tobin <tobster111@gmail.com>
2022-08-02 18:15:37 +0100

    Array types

--- Learning-Typescript/Arrays/index.ts
+++ Learning-Typescript/Arrays/index.ts
@@ -4,3 +4,7 @@ warriors.push('Antman'); // Ok
 
 warriors.push(true);
 // Error: Argument of type 'true' is not assignable to parameter of type 'string'.
+
+// * ARRAY TYPES
+let arrayOfNumbers: number[];
+arrayOfNumbers = [1, 2, 3];
