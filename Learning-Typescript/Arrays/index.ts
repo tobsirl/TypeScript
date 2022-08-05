bdea2b7395ebc38c8b6ee9bae89ce60f277348ea
Paul Tobin <tobster111@gmail.com>
2022-08-05 17:46:27 +0100

    logPair Correct

--- Learning-Typescript/Arrays/index.ts
+++ Learning-Typescript/Arrays/index.ts
@@ -122,3 +122,12 @@ const pairArray = ['Amage', 1];
 logPair(...pairArray);
 // Error: A spread argument must either have a
 // tuple type or be passed to a rest parameter.
+
+const pairTupleIncorrect: [number, string] = [1, 'Amage'];
+
+logPair(...pairTupleIncorrect);
+// Error: Argument of type 'number' is not
+// assignable to parameter of type 'string'.
+
+const pairTupleCorrect: [string, number] = ['Amage', 1];
+logPair(...pairTupleCorrect); // Ok
