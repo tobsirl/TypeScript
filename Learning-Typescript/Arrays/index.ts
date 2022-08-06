cb488e969b459449a97d705c17530f30718a5dd2
Paul Tobin <tobster111@gmail.com>
2022-08-06 13:51:05 +0100

    Arrays finished

--- Learning-Typescript/Arrays/index.ts
+++ Learning-Typescript/Arrays/index.ts
@@ -168,7 +168,7 @@ function firstCharAndSizeExplicit(input: string): [string, number] {
 
 // firstChar type: string
 // size type: number
-const [firstChar, size] = firstCharAndSizeExplicit('Cathay Williams');
+// const [firstChar, size] = firstCharAndSizeExplicit('Cathay Williams');
 
 // * Const asserted tuples
 // Type: (string | number)[]
@@ -176,3 +176,23 @@ const unionArray = [1157, 'Tomoe'];
 
 // Type: readonly [1157, 'Tomoe']
 const readonlyTuple = [1157, 'Tomoe'] as const;
+
+const pairMutable: [number, string] = [1157, 'Tomoe'];
+pairMutable[0] = 1247; // Ok
+
+const pairAlsoMutable: [number, string] = [1157, 'Tomoe'] as const;
+// Error: The type 'readonly [1157, "Tomoe"]' is 'readonly'
+// and cannot be assigned to the mutable variable '[number, string]'.
+
+const pairConst = [1157, 'Tomoe'] as const;
+pairConst[0] = 1247;
+// Error: Cannot assign to '0' because it is a read-only property.
+
+// Return type: readonly [string, number]
+function firstCharAndSizeAsConst(input: string) {
+  return [input[0], input.length] as const;
+}
+
+// firstChar type: string
+// size type: number
+const [firstChar, size] = firstCharAndSizeAsConst('Cathay Williams');
