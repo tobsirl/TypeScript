37175a4a2da39a7d24b5e901708d40fd8d606c3f
Paul Tobin <tobster111@gmail.com>
2022-08-08 20:19:55 +0100

    valueLater

--- Learning-Typescript/Interfaces/index.ts
+++ Learning-Typescript/Interfaces/index.ts
@@ -9,3 +9,14 @@ interface Poet {
   born: number;
   name: string;
 }
+
+let valueLater: Poet;
+
+// Ok
+valueLater = {
+  born: 1935,
+  name: 'Sara Teasdale',
+};
+
+valueLater = 'Emily Dickinson';
+// Error: Type 'string' is not assignable to type 'Poet'.
