4c942375286ed50c2c8d3b1fa973fd45cbeb9ae9
Paul Tobin <tobster111@gmail.com>
2022-08-12 15:54:46 +0100

    Call Signature example

--- Learning-Typescript/Interfaces/index.ts
+++ Learning-Typescript/Interfaces/index.ts
@@ -101,3 +101,27 @@ const typedFunctionAlias: FunctionAlias = (input) => input.length; // Ok
 
 // Type: (input: string) => number
 const typedCallSignature: CallSignature = (input) => input.length; // Ok
+
+interface FunctionWithCount {
+  count: number;
+  (): void;
+}
+
+let hasCallCount: FunctionWithCount;
+
+function keepsTrackOfCalls() {
+  keepsTrackOfCalls.count += 1;
+  console.log(`I've been called ${keepsTrackOfCalls.count} times!`);
+}
+
+keepsTrackOfCalls.count = 0;
+
+hasCallCount = keepsTrackOfCalls; // Ok
+
+function doesNotHaveCount() {
+  console.log('No idea!');
+}
+
+hasCallCount = doesNotHaveCount;
+// Error: Property 'count' is missing in type
+// '() => void' but required in type 'FunctionWithCalls'.
