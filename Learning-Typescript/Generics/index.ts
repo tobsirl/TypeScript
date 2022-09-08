7c0db48e05a1d1e9d969892f0d34e845a4770aa7
Paul Tobin <tobster111@gmail.com>
2022-09-08 18:13:30 +0100

    Generic function

--- Learning-Typescript/Generics/index.ts
+++ Learning-Typescript/Generics/index.ts
@@ -2,3 +2,10 @@
  * GENERICS
  */
 
+// Generic function
+function identity<T>(input: T): T {
+  return input;
+}
+
+const numeric = identity('me'); // Type: "me"
+const stringy = identity(123); // Type: 123
