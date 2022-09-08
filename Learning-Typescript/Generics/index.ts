9160737dca0d9c852f5bc84255f4bd0938f4cb51
Paul Tobin <tobster111@gmail.com>
2022-09-08 18:16:39 +0100

    logWrapper

--- Learning-Typescript/Generics/index.ts
+++ Learning-Typescript/Generics/index.ts
@@ -9,3 +9,11 @@ function identity<T>(input: T): T {
 
 const numeric = identity('me'); // Type: "me"
 const stringy = identity(123); // Type: 123
+
+//  Explicit Generic Call Types
+function logWrapper<Input>(callback: (input: Input) => void) {
+  return (input: Input) => {
+    console.log('Input: ', input);
+    callback(input);
+  };
+}
